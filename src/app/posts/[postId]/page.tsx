e5064ef852cea78b3edd77ecc88506c2a40d184e
AB1738 <saintsring2017@yahoo.com>
2025-04-04 19:03:34 -0400

    updated post page

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -1,7 +1,7 @@
-import { Button } from "@/components/ui/button";
 import { prisma } from "../../../../lib/prisma";
-import { redirect } from 'next/navigation'
+import { redirect } from "next/navigation";
 import BackButton from "@/components/customComponents/BackButton";
+import { UserRoundPen, CalendarClock } from "lucide-react";
 
 interface postIdPropType {
   params: {
@@ -9,7 +9,7 @@ interface postIdPropType {
   };
 }
 const page = async ({ params }: postIdPropType) => {
-  const { postId } = params
+  const { postId } = params;
   const post = await prisma.blogPost.findUnique({
     where: {
       id: postId,
@@ -22,18 +22,34 @@ const page = async ({ params }: postIdPropType) => {
       },
     },
   });
-  if(!post){
-    redirect('/posts')
+  if (!post) {
+    redirect("/posts");
   }
   console.log(post);
-  return <div>
-    <BackButton/>
-    <h1>{post?.title}</h1>
-    <h2>{post.description}</h2>
-    <h3>{post.blogPostText}</h3>
-    <p>{post.author.username.charAt(0).toUpperCase()+post.author.username.slice(1)}</p>
-    <p>{post.updatedAt.toLocaleString()}</p>
-  </div>;
+  return (
+    <div className="pt-5">
+      <BackButton />
+      <div className="flex justify-between items-center my-3">
+        <h1 className="text-3xl font-bold">
+          {post?.title.charAt(0).toUpperCase() + post.title.slice(1)}
+        </h1>
+        <p className="text-sm flex gap-0.5">
+          {post.author.username.charAt(0).toUpperCase() +
+            post.author.username.slice(1)}
+          <UserRoundPen size={16} />
+        </p>
+      </div>
+      <h2 className="my-2">{post.description}</h2>
+
+      <h3>
+        {post.blogPostText.charAt(0).toUpperCase() + post.blogPostText.slice(1)}
+      </h3>
+      <p className="flex justify-end items-center text-xs  gap-0.5 mt-2.5">
+        {post.updatedAt.toLocaleString()}
+        <CalendarClock size={16} />
+      </p>
+    </div>
+  );
 };
 
 export default page;
--- src/components/customComponents/BackButton.tsx
+++ src/components/customComponents/BackButton.tsx
@@ -2,11 +2,13 @@
 
 import { useRouter } from "next/navigation"
 import { Button } from "../ui/button"
+import {ArrowLeft} from 'lucide-react'
 
 const BackButton = () => {
    const router= useRouter()
   return (
     <Button onClick={()=>router.back()} variant={'ghost'} className="cursor-pointer hover:scale-102">
+        <ArrowLeft />
         Back To Posts
     </Button>
   )
