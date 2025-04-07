5ae0ae99dee83ac862589a6fa70b8b2fc5ca7530
AB1738 <saintsring2017@yahoo.com>
2025-04-07 17:10:11 -0400

    updated post page

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -27,19 +27,21 @@ const page = async ({ params }: postIdPropType) => {
   }
   console.log(post);
   return (
-    <div className="pt-5">
+    <div className="py-5 text-center sm:text-left">
+        <div className="text-left">
       <BackButton />
-      <div className="flex justify-between items-center my-3">
-        <h1 className="text-3xl font-bold">
+      </div>
+      <div className="flex justify-between items-center text-center gap-2 sm:text-left my-3">
+        <h1 className="text-2xl sm:text-3xl font-bold">
           {post?.title.charAt(0).toUpperCase() + post.title.slice(1)}
         </h1>
-        <p className="text-sm flex gap-0.5">
+        <p className="text-sm flex gap-0.5 self-end">
           {post.author.username.charAt(0).toUpperCase() +
             post.author.username.slice(1)}
           <UserRoundPen size={16} />
         </p>
       </div>
-      <h2 className="my-2">{post.description}</h2>
+      <h2 className="my-2">{post.description.charAt(0).toUpperCase()+post.description.slice(1)}</h2>
 
       <h3>
         {post.blogPostText.charAt(0).toUpperCase() + post.blogPostText.slice(1)}
